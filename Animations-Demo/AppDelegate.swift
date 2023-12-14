6e7b7cefc161c672870096b37e09106fa297ff33
SebD <sebastian.detering@gmail.com>
2023-12-14 15:20:37 -0800

    Initial Commit

---
+++ Animations-Demo/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Animations-Demo
+//
+//  Created by sebi d on 12/14/23.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
