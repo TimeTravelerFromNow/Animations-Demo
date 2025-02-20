a3bd53406b55ab886c5adeaf4ab87979bb829d94
Sebastian Detering <98575184+TimeTravelerFromNow@users.noreply.github.com>
2025-02-20 08:21:04 -0800

    Offset floating label on left of subject node

--- Animations-Demo/CustomNodes/CustomFloatingLabel.swift
+++ Animations-Demo/CustomNodes/CustomFloatingLabel.swift
@@ -2,6 +2,8 @@ import SpriteKit
 
 // FloatingUI style description, title
 class CustomFloatingLabel: SKNode {
+    let _WIDTH: CGFloat!
+    let _PADDING: CGFloat! // == margin
     weak var subject: SKNode?
     var title: String
     var descriptionText: String
@@ -13,13 +15,19 @@ class CustomFloatingLabel: SKNode {
     var descriptionNode: SKLabelNode!
     var descriptionBackground: SKShapeNode!
     
-    init(title: String, text: String, subject: SKNode? = nil) {
+    init(title: String, text: String, subject: SKNode? = nil, width: CGFloat = 200, padding: CGFloat = 10) {
         self.subject = subject
         self.title = title
         self.descriptionText = text
+        self._WIDTH = width
+        self._PADDING = padding
         super.init()
         
-        buildFloatingTitle(position: subject?.position)
+        // left of subject
+        var position = subject == nil ? nil : CGPoint(x: subject!.position.x - subject!.frame.minX, y: subject!.position.y)
+        
+        buildFloatingTitle(position: position)
+        
         var descriptionPosition = self.titleNode.position
         buildFloatingDescription(position: descriptionPosition)
     }
@@ -28,14 +36,19 @@ class CustomFloatingLabel: SKNode {
         self.titleNode = SKLabelNode(fontNamed: "Chalkduster")
         
         self.titleNode.position = position ?? CGPoint(x:0,y:0)
+        
         self.titleNode.text = self.title
         var titleRect = titleNode.frame.insetBy(dx: -10, dy: -10)
-        self.titleBackground = SKShapeNode(rect: titleRect, cornerRadius: 10)
+        // override size with _WIDTH constant
+        var titleSize = CGSize(width: _WIDTH, height: titleRect.height + 10)
+        self.titleBackground = SKShapeNode(rectOf: titleSize, cornerRadius: 10)
         titleBackground.fillColor = .white
         titleNode.fontColor = .black
+        titleNode.verticalAlignmentMode = .center
         
         self.addChild(titleNode)
-        self.addChild(titleBackground)
+        titleBackground.zPosition = -1
+        titleNode.addChild(titleBackground)
     }
     
     func buildFloatingDescription(position: CGPoint?) {
@@ -48,10 +61,9 @@ class CustomFloatingLabel: SKNode {
         self.descriptionNode.numberOfLines = 0
 
         self.descriptionNode.lineBreakMode = .byWordWrapping
-        self.descriptionNode.preferredMaxLayoutWidth = 200
-        self.descriptionNode.verticalAlignmentMode = .top
+        self.descriptionNode.preferredMaxLayoutWidth = _WIDTH
+        self.descriptionNode.verticalAlignmentMode = .center
 
-        
         self.descriptionNode.text = self.descriptionText
         self.descriptionNode.fontSize = 12
         
@@ -60,10 +72,22 @@ class CustomFloatingLabel: SKNode {
         descriptionBackground.fillColor = .white
         descriptionNode.fontColor = .black
         
-        descriptionNode.position.y -= 40
+        descriptionNode.position.y -= 50
         self.addChild(descriptionNode)
         descriptionBackground.zPosition = -1
         descriptionNode.addChild(descriptionBackground)
+    }
+    
+    
+    private let _fIA: SKAction = SKAction.fadeIn(withDuration: 1)
+    let _DELAY: TimeInterval = 1
+    func fadeIn(wait: Bool = false) {
+        if wait {
+            let delayA = SKAction.wait(forDuration: _DELAY)
+            self.run( SKAction.group([ delayA, _fIA]) )
+        } else {
+            self.run(_fIA)
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
